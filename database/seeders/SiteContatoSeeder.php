474edca934b143838ea7f314addda5604c8b2073
CodeAkio <siqueira_2004@hotmail.com>
2022-05-15 19:42:02 -0300

    Migrations, models, factories and seeders

---
+++ database/seeders/SiteContatoSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SiteContato;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SiteContatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        SiteContato::factory(100)->create();
+    }
+}

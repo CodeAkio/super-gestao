8d59b2de87f1e5cbf392feb13d6215c3cea21505
CodeAkio <siqueira_2004@hotmail.com>
2022-06-09 23:39:00 -0300

    form request

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
+use App\Models\MotivoContato;
 
 class ContatoController extends Controller
 {
     function contato()
     {
-        return view('site.contato');
+        $motivo_contatos = MotivoContato::all();
+
+        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
+    }
+
+    function salvar(Request $request)
+    {
+        $regras = [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
+            'telefone' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
+            'mensagem' => 'required|max:2000',
+        ];
+
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.min' => 'O campo nome dev ter no máximo 40 caracteres',
+            'nome.unique' => 'O nome informado já existe',
+
+            'email.email' => 'O e-mail informado não é valido',
+
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+
+            'required' => 'O campo :attribute precisa ser preenchido',
+        ];
+
+        $request->validate($regras, $feedback);
+
+        SiteContato::create($request->all());
+
+        return redirect()->route('site.index');
     }
 }
